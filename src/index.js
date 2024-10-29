2c689bc956df216a95b47fd12c79ddc2c485f0b1
RoyMelvinT <roymelvin21@gmail.com>
2024-10-29 11:31:31 -0500

    nodejs

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import {dirname, join} from 'path'
+import { fileURLToPath } from 'url'
+
+const app= express()
+
+const __dirname= dirname(fileURLToPath(import.meta.url))
+console.log(join(__dirname, 'views'))
+app.set('views', join(__dirname, 'views'))
+
+
+// examencv_app/src/views
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('index'))
+
+app.listen(3000)
+console.log("el servidor esta corriendo en el puerto: ",3000)
