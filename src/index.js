a12201b5d7184d26b24b8f4fbf3c9305f636a33a
RoyMelvinT <roymelvin21@gmail.com>
2024-10-29 12:52:56 -0500

    cambiando el app por router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {dirname, join} from 'path'
 import { fileURLToPath } from 'url'
+import indexRouter from './router/index.js'
 
 const app= express()
 
@@ -13,7 +14,7 @@ app.set('views', join(__dirname, 'views'))
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('index'))
 
-app.listen(3000)
-console.log("el servidor esta corriendo en el puerto: ",3000)
+
+app.listen(8085)
+console.log("el servidor esta corriendo en el puerto: ",8085)
